b9d6d0d45a4f89d1b902c855e8ed43e5e5e40c14
Gustavo Marinho <ghmarinho@gmail.com>
2026-08-01 10:59:27 -0300

    aula-2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { PageNotFound } from './page-not-found/page-not-found';
+import { Cadastro } from './cadastro/cadastro';
+import { Dashboard } from './dashboard/dashboard';
+import { Despesas } from './relatorios/despesas/despesas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: Login},
+    {path: 'cadastro', component: Cadastro},
+    {path: 'dashboard', component: Dashboard},
+    {path: 'relatorios-despesa', component: Despesas},
+    {path: '**', component: PageNotFound },
+];
